e222af94410916bbb42d46acb89fa333cb16007b
elad-rahamim <elad@eready.co.il>
2015-05-04 23:26:47 +0300

    add pages for Apps

---
+++ Slide-left-menu/Slide-left-menu/Apps/Pages/About/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.swift
+//  Slide-left-menu
+//
+//  Created by Elad Rahmim on 5/4/15.
+//  Copyright (c) 2015 Elad Rahmim. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
